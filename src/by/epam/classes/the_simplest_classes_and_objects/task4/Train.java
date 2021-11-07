a390299e696a2ff0a1a5999371b0b61307d57785
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-07 16:32:25 +0200

    Create Train.java

---
+++ src/by/epam/classes/the_simplest_classes_and_objects/task4/Train.java
@@ -0,0 +1,58 @@
+/*
+4.  
+- Создайте  класс  Train,  содержащий  поля:  название  пункта  назначения,  номер  поезда,  время  отправления. 
+- Создайте данные в массив из пяти элементов типа Train, добавьте возможность сортировки элементов массива по 
+номерам поездов. 
+- Добавьте возможность вывода информации о поезде, номер которого введен пользователем. 
+- Добавьте  возможность  сортировки  массив  по  пункту  назначения,  причем  поезда  с  одинаковыми  пунктами 
+назначения должны быть упорядочены по времени отправления. 
+*/
+
+package by.epam.classes.the_simplest_classes_and_objects;
+
+import java.util.Comparator;
+
+public class Train {
+    
+    private String destination;
+    private int trainNumber;
+    private String departureTime;
+    
+    public Train(String destination, int trainNumber, String departureTime) 
+    {
+        this.destination = destination;
+        this.trainNumber = trainNumber;
+        this.departureTime = departureTime;
+    }
+    
+    public static class ByNumberComparator implements Comparator < Train > 
+    {
+        @Override
+        public int compare(Train left, Train right) 
+        {
+            return left.trainNumber - right.trainNumber;
+        }
+    }
+    
+    public static class ByDestinationAndTimeComparator implements Comparator < Train > 
+    {
+        @Override
+        public int compare(Train left, Train right) 
+        {
+            if (left.destination == right.destination)
+                return left.departureTime.compareTo(right.departureTime);
+                return left.destination.compareTo(right.destination);
+        }
+    }
+    
+    public int getTrainNumber() 
+    {
+        return trainNumber;
+    }
+    
+    public void print()
+    {
+        System.out.println(trainNumber + " -> " + destination + " : " + departureTime);
+    }
+    
+}
