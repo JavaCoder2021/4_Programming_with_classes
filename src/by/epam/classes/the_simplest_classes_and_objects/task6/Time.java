b4796203cbe7ee6cb8a622dbbe86bc90d14371bd
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-10 21:29:59 +0200

    Update Time.java

--- src/by/epam/classes/the_simplest_classes_and_objects/task6/Time.java
+++ src/by/epam/classes/the_simplest_classes_and_objects/task6/Time.java
@@ -20,62 +20,52 @@ public class Time {
     
     public void setTime(int hours_, int min_, int sec_)
     {
-        if (sec_ >=0 && sec_ < 60)
-            sec = sec_;
-        else
-            sec = 0;
-        if (min_ >=0 && min_ < 60)
-            min = min_;
-        else
-            min = 0;
-        if (hours_ >=0 && hours_ < 24)
-            hours = hours_;
-        else
-            hours = 0;
+        sec = (sec_ >=0 && sec_ < 60) ? sec_ : 0;
+        min = (min_ >=0 && min_ < 60) ? min_ : 0;
+        hours = (hours_ >=0 && hours_ < 24) ? hours_ : 0;
     }
     
     public void setSec(int sec_)
     {
-        if (sec_ >=0 && sec_ < 60)
-            sec = sec_;
-        else
-            sec = 0;
+        sec = (sec_ >=0 && sec_ < 60) ? sec_ : 0;
     }
     
     public void setMin(int min_)
     {
-        if (min_ >=0 && min_ < 60)
-            min = min_;
-        else
-            min = 0;           
+        min = (min_ >=0 && min_ < 60) ? min_ : 0;           
     }
     
     public void setHours(int hours_)
     {
-        if (hours_ >=0 && hours_ < 24)
-            hours = hours_;
-        else
-            hours = 0;         
-    }   
+        hours = (hours_ >=0 && hours_ < 24) ? hours_ : 0;       
+    }  
+    
+    public void addSeconds(int sec_)
+    {
+        sec += sec_;
+        
+        min += sec / 60;
+        sec = sec % 60;
+        
+        hours += min / 60;
+        min = min % 60;
+        
+        hours = hours % 24;
+    }
+    
+    public void addMinutes(int min_)
+    {
+        addSeconds(min_ * 60);
+    }
+ 
+    public void addHours(int hours_)
+    {
+        addSeconds(hours_ * 60 * 60);
+    }
     
     public void addTime(int hours_, int min_, int sec_)
     {
-        if (sec_ >=0)
-        {
-            sec += sec_ % 60;
-            min += (sec_ / 60) % 60;
-        }
-        if (min_ >=0)
-        {
-            min += min_ % 60;
-            hours += (min_ / 60) % 60;
-        }
-        if (hours_ >=0)
-        {
-            hours += hours_;
-            if (hours >= 24)
-                hours = hours % 24;
-        }
+        addSeconds(hours_ * 60 * 60 + min_ * 60 + sec_);
     }
     
     public void showTime()
