01f36efb3f89fe0ab0073cca304bd43eb82366af
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-13 22:05:31 +0200

    Create JavaCoder2021.java

---
+++ src/by/epam/classes/the_simplest_classes_and_objects/task9/JavaCoder2021.java
@@ -0,0 +1,57 @@
+package javacoder2021;
+
+import by.epam.classes.the_simplest_classes_and_objects.*;
+import java.util.*;
+
+public class JavaCoder2021 {
+
+    public static void main(String[] args) {
+        
+        Books books = new Books();
+        books.addBooks();
+    
+        Scanner input = new Scanner(System.in);
+        int choice;
+        
+        while (true)
+        {
+            menuText();
+            choice = Integer.parseInt(input.nextLine());
+            if (choice == 0)
+                break;
+            if (choice < 0 || choice > 3) 
+            {
+                System.out.println("Incorrect choose!");
+                continue;
+            }
+                      
+            switch (choice)
+            {
+                case 1:
+                    books.sortByAuthor(input);
+                    break;           
+                case 2:
+                    books.sortByPublisher(input);
+                    break;           
+                case 3:
+                    books.sortByYear(input);
+                    break;             
+            }     
+            
+        }    
+        
+    }
+    
+    private static void menuText() { 
+        System.out.print(
+            "\n" + 
+            "Выберете пункт меню:\n" +
+            "0. Выйти\n" +
+            "1. Вывести книги данного автора\n" +
+            "2. Вывести книги, выпущенные заданным издательством\n" +
+            "3. Вывести книги, выпущенные после заданного года\n" +
+            "\n"
+        );   
+    }
+    
+}
