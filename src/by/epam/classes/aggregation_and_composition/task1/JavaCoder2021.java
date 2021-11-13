7c6938c56f624f0cae268bf56d3f38c3c82ab3a2
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-14 00:32:57 +0200

    Create JavaCoder2021.java

---
+++ src/by/epam/classes/aggregation_and_composition/task1/JavaCoder2021.java
@@ -0,0 +1,34 @@
+package javacoder2021;
+
+import by.epam.classes.the_simplest_classes_and_objects.*;
+
+public class JavaCoder2021 {
+
+    public static void main(String[] args) {
+        
+        Word word1 = new Word("Train");
+        Word word2 = new Word();
+        Word word3 = new Word();
+        word2.setValue("let's");
+        word3.setValue("go");
+        
+        Sentence sentense1 = new Sentence();
+        sentense1.addWord(word1);
+        sentense1.addWord(word2);
+        sentense1.addWord(word3);
+        sentense1.print();
+        System.out.println("\n");
+        
+        Word word4 = new Word("Green");
+        Word word5 = new Word("tree");
+        Sentence sentense2 = new Sentence();
+        sentense2.addWord(word4);
+        sentense2.addWord(word5);  
+        
+        Text text = new Text();
+        text.setHead("Train");
+        text.addSentence(sentense1);
+        text.addSentence(sentense2);
+        text.print();
+    }
+}
