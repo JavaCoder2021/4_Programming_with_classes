70f9c7283d698c79836f182eb9b0b1d9156b6405
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-14 19:53:55 +0200

    Create City.java

---
+++ src/by/epam/classes/aggregation_and_composition/task3/City.java
@@ -0,0 +1,42 @@
+/*
+3. 
+Создать объект класса Государство, используя классы Область, Район, Город. 
+Методы: вывести на консоль столицу, количество областей, площадь, областные центры.  
+*/
+
+package by.epam.classes.the_simplest_classes_and_objects;
+
+public class City {
+    
+    private String district, city;
+ 
+    public City(String city) 
+    {
+        this.city = city;
+    }
+ 
+    public String getCity() 
+    {
+        return city;
+    }
+    
+    public String getDistrict() 
+    {
+        return district;
+    }
+ 
+    public static City addCity()
+    {
+        City city = new City("");
+        System.out.println("Add city: ");
+        System.out.println("---------------------------");
+        System.out.print("District name: ");
+        city.district = Country.strInput();
+        System.out.print("City name: ");
+        city.city = Country.strInput();
+        System.out.println();
+        
+        return city;
+    }
+    
+}
