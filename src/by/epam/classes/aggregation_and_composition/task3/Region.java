6d1e1c35f94a39f4fe300ac55a1f2a4762688d87
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-14 19:54:53 +0200

    Create Region.java

---
+++ src/by/epam/classes/aggregation_and_composition/task3/Region.java
@@ -0,0 +1,47 @@
+package by.epam.classes.the_simplest_classes_and_objects;
+
+import java.util.*;
+
+public class Region {
+    
+    private String region, regionCity;
+    private double square;
+ 
+    public Region(String region, String regionCity, double square) 
+    {
+        this.region = region;
+        this.square = square;
+    }
+ 
+    public String getRegion() 
+    {
+        return region;
+    }
+    
+   public String getRegionCity() 
+    {
+        return regionCity;
+    }
+ 
+    public double getSquare() 
+    {
+        return square;
+    }
+ 
+    public static Region addRegion() 
+    {
+        Region region = new Region("", "", 0);
+        System.out.println("Add region: ");
+        System.out.println("---------------------------");        
+        System.out.print("Region name: ");
+        region.region = Country.strInput();
+        System.out.print("Region city: ");
+        region.regionCity = Country.strInput();
+        System.out.print("Square (km2): ");
+        region.square = Country.doubleInput();
+        System.out.println();
+        
+        return region;
+    }
+    
+}
