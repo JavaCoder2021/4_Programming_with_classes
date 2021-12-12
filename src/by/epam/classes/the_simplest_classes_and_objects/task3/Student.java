bf425dcd1550158f8af3a82468fcc91e4e7d5409
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-12-12 18:21:32 +0200

    Update Student.java

--- src/by/epam/classes/the_simplest_classes_and_objects/task3/Student.java
+++ src/by/epam/classes/the_simplest_classes_and_objects/task3/Student.java
@@ -1,12 +1,13 @@
 /*
-3. 
-- Создайте класс с именем Student, содержащий поля: 
-- фамилия и инициалы, номер группы, успеваемость (массив из пяти элементов). 
-- Создайте массив из десяти элементов такого типа. 
-- Добавьте возможность вывода фамилий и номеров групп студентов, имеющих оценки, равные только 9 или 10.
-*/
+ * 3. 
+ * - Создайте класс с именем Student, содержащий поля: 
+ * - фамилия и инициалы, номер группы, успеваемость (массив из пяти элементов). 
+ * - Создайте массив из десяти элементов такого типа. 
+ * - Добавьте возможность вывода фамилий и номеров групп студентов, имеющих оценки, равные только 9 или 10.
+ */
+package by.epam.classes.the_simplest_classes_and_objects.task3;
 
-package by.epam.classes.the_simplest_classes_and_objects;
+import java.util.Arrays;
 
 public class Student {
     
@@ -14,30 +15,24 @@ public class Student {
     private int groupNumber;
     private int[] academicPerformance = new int[5];  
     
-    public Student(String fio, int groupNumber, int[] academicPerformance)
-    {
+    public Student(String fio, int groupNumber, int[] academicPerformance) {
         this.fio = fio;
         this.groupNumber = groupNumber;
         this.academicPerformance = academicPerformance;
     }
     
-    public boolean isGood()
-    {
+    public boolean isGood() {
         boolean bestSt;
         bestSt = true;
-        for (int i : academicPerformance)
-        {
-                if (i < 9)
-                    bestSt = false;
-        }
-        
+        for (int i : academicPerformance) {
+            if (i < 9) bestSt = false;
+        }  
         return bestSt;
-
     }
     
-    public void print()
-    {
-        System.out.printf("%s, №%d \n", fio, groupNumber);  
+    @Override
+    public String toString() {
+        return fio + " №" + groupNumber;
     }
     
 }
