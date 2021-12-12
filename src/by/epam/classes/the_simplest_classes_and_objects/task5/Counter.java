1b921699408fb1a66644ad24e0eeee1f14f02759
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-12-12 18:42:29 +0200

    Update Counter.java

--- src/by/epam/classes/the_simplest_classes_and_objects/task5/Counter.java
+++ src/by/epam/classes/the_simplest_classes_and_objects/task5/Counter.java
@@ -1,25 +1,22 @@
 /*
-5. 
-Опишите класс, реализующий десятичный счетчик, который может увеличивать или уменьшать свое значение 
-на  единицу  в  заданном  диапазоне.  Предусмотрите инициализацию  счетчика  значениями  по  умолчанию  и 
-произвольными  значениями. Счетчик  имеет  методы  увеличения  и  уменьшения  состояния,  и  метод 
-позволяющее получить его текущее состояние. Написать код, демонстрирующий все возможности класса. 
-*/
-
-package by.epam.classes.the_simplest_classes_and_objects;
+ * 5. 
+ * Опишите класс, реализующий десятичный счетчик, который может увеличивать или уменьшать свое значение 
+ * на  единицу  в  заданном  диапазоне.  Предусмотрите инициализацию  счетчика  значениями  по  умолчанию  и 
+ * произвольными  значениями. Счетчик  имеет  методы  увеличения  и  уменьшения  состояния,  и  метод 
+ * позволяющее получить его текущее состояние. Написать код, демонстрирующий все возможности класса. 
+ */
+package by.epam.classes.the_simplest_classes_and_objects.task5;
 
 public class Counter {
     
     private int min, max, current;
     
-    public Counter(int min_, int max_, int current_) 
-    {
+    public Counter(int min_, int max_, int current_) {
         min = min_;
         max = max_;
         current = current_;
         
-        if (max < min) 
-        {
+        if (max < min) {
             int tmp = max;
             max = min;
             min = tmp;
@@ -33,8 +30,7 @@ public class Counter {
             current = max;      
     }  
     
-    public Counter(int min_, int max_) 
-    {
+    public Counter(int min_, int max_) {
         this(min_, max_, min_);      
     }
     
@@ -42,21 +38,18 @@ public class Counter {
         this(0, 16, 0);
     }
     
-    public void inc() 
-    {
+    public void inc() {
         current++;
         if (current > max)
             current = min;
     }
-    public void dec() 
-    {
+    public void dec() {
         current--;
         if (current < min)
             current = max;
     }
  
-    public int value() 
-    {
+    public int value() {
         return current;
     }    
     
