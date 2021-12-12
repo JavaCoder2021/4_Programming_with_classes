3a47fbfb3f30065f1598b3d429d11d55c4bc0d53
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-12-12 21:48:37 +0200

    Update Sentence.java

--- src/by/epam/classes/aggregation_and_composition/task1/Sentence.java
+++ src/by/epam/classes/aggregation_and_composition/task1/Sentence.java
@@ -1,4 +1,4 @@
-package by.epam.classes.aggregation_and_composition;
+package by.epam.classes.aggregation_and_composition.task1;
 
 import java.util.*;
 
@@ -6,25 +6,20 @@ public class Sentence {
     
     private ArrayList<String> words = new ArrayList<>();
 
-    public Sentence()
-    {
+    public Sentence() {
         //
     }
     
-    public Sentence(Word word)
-    {
+    public Sentence(Word word) {
         addWord(word);
     }
         
-    public void addWord(Word word)
-    {
+    public void addWord(Word word) {
         this.words.add(word.getValue());
     }
     
-    public void print()
-    {
-        for (int i = 0; i < words.size(); i++)
-        {
+    public void print() {
+        for (int i = 0; i < words.size(); i++) {
             if ((i + 1) == words.size())
                 System.out.print(words.get(i) + ". ");
             else
