e063b577e1c1c2e6d54dd5b2ad67e8af92880a5a
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-12-12 21:36:07 +0200

    Update Airlines.java

--- src/by/epam/classes/the_simplest_classes_and_objects/task10/Airlines.java
+++ src/by/epam/classes/the_simplest_classes_and_objects/task10/Airlines.java
@@ -1,4 +1,4 @@
-package by.epam.classes.the_simplest_classes_and_objects;
+package by.epam.classes.the_simplest_classes_and_objects.task10;
 
 import java.util.*;
 
@@ -6,13 +6,7 @@ public class Airlines {
     
     LinkedList<Airline> airlines = new LinkedList();
     
-    public Airlines()
-    {
-        addAirlines();
-    }
-    
-    public void addAirlines() 
-    {
+    public Airlines() {
         airlines.add(new Airline("Moscow",  105,    "Boing",    "13.55", "Monday"));
         airlines.add(new Airline("Minsk",   35,     "Boing",    "10.05", "Saturday"));
         airlines.add(new Airline("Kiev",    125,    "Boing",    "09.20", "Sunday"));
@@ -20,35 +14,31 @@ public class Airlines {
         airlines.add(new Airline("Minsk",   45,     "Boing",    "19.15", "Wednesday"));
     }
        
-    public void sortByDestination(Scanner input) 
-    {
+    public void sortByDestination(Scanner input) {
         System.out.print("Destination: ");
         String destination = input.nextLine();
         for (Airline mbr : airlines) 
-            if (destination.equals(mbr.getDestination()))
+            if (destination.toLowerCase().equals(mbr.getDestination().toLowerCase()))
                 mbr.print();
     }
     
-    public void sortByDaysOfWeek(Scanner input) 
-    {
+    public void sortByDaysOfWeek(Scanner input) {
         System.out.print("Day of week: ");
         String daysOfWeek = input.nextLine();
-        for (Airline mbr : airlines) 
-        {    
-            if (daysOfWeek.equals(mbr.getDaysOfWeek()))
+        for (Airline mbr : airlines) {    
+            if (daysOfWeek.toLowerCase().equals(mbr.getDaysOfWeek().toLowerCase()))
                 mbr.print();
         }
     }
     
-    public void sortByDepartureTime(Scanner input) 
-    {
+    public void sortByDepartureTime(Scanner input) {
         System.out.print("Day of week: ");
         String daysOfWeek = input.nextLine();
         System.out.print("Departure time: ");
         String departureTime = input.nextLine();
-        for (Airline mbr : airlines) 
-        {    
-            if (daysOfWeek.equals(mbr.getDaysOfWeek()) && (Double.valueOf(mbr.getDepartureTime()) > Double.valueOf(departureTime)))
+        for (Airline mbr : airlines) {    
+            if (daysOfWeek.toLowerCase().equals(mbr.getDaysOfWeek().toLowerCase()) && 
+                    (Double.valueOf(mbr.getDepartureTime()) > Double.valueOf(departureTime)))
                 mbr.print();
         }
     }    
