9a5b6f308d3f19e8dccd208d5904e3248ddadf29
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-12-12 18:35:13 +0200

    Update Train.java

--- src/by/epam/classes/the_simplest_classes_and_objects/task4/Train.java
+++ src/by/epam/classes/the_simplest_classes_and_objects/task4/Train.java
@@ -1,14 +1,13 @@
 /*
-4.  
-- Создайте  класс  Train,  содержащий  поля:  название  пункта  назначения,  номер  поезда,  время  отправления. 
-- Создайте данные в массив из пяти элементов типа Train, добавьте возможность сортировки элементов массива по 
-номерам поездов. 
-- Добавьте возможность вывода информации о поезде, номер которого введен пользователем. 
-- Добавьте  возможность  сортировки  массив  по  пункту  назначения,  причем  поезда  с  одинаковыми  пунктами 
-назначения должны быть упорядочены по времени отправления. 
-*/
-
-package by.epam.classes.the_simplest_classes_and_objects;
+ * 4.  
+ * - Создайте  класс  Train,  содержащий  поля:  название  пункта  назначения,  номер  поезда,  время  отправления. 
+ * - Создайте данные в массив из пяти элементов типа Train, 
+ * добавьте возможность сортировки элементов массива по номерам поездов. 
+ * - Добавьте возможность вывода информации о поезде, номер которого введен пользователем. 
+ * - Добавьте  возможность  сортировки  массив  по  пункту  назначения,  причем  поезда  с  одинаковыми  пунктами 
+ * назначения должны быть упорядочены по времени отправления. 
+ */
+package by.epam.classes.the_simplest_classes_and_objects.task4;
 
 import java.util.Comparator;
 
@@ -18,40 +17,33 @@ public class Train {
     private int trainNumber;
     private String departureTime;
     
-    public Train(String destination, int trainNumber, String departureTime) 
-    {
+    public Train(String destination, int trainNumber, String departureTime) {
         this.destination = destination;
         this.trainNumber = trainNumber;
         this.departureTime = departureTime;
     }
     
-    public static class ByNumberComparator implements Comparator < Train > 
-    {
+    public static class ByNumberComparator implements Comparator < Train > {
         @Override
-        public int compare(Train left, Train right) 
-        {
+        public int compare(Train left, Train right) {
             return left.trainNumber - right.trainNumber;
         }
     }
     
-    public static class ByDestinationAndTimeComparator implements Comparator < Train > 
-    {
+    public static class ByDestinationAndTimeComparator implements Comparator < Train > {
         @Override
-        public int compare(Train left, Train right) 
-        {
+        public int compare(Train left, Train right) {
             if (left.destination == right.destination)
                 return left.departureTime.compareTo(right.departureTime);
                 return left.destination.compareTo(right.destination);
         }
     }
     
-    public int getTrainNumber() 
-    {
+    public int getTrainNumber() {
         return trainNumber;
     }
     
-    public void print()
-    {
+    public void print() {
         System.out.println(trainNumber + " -> " + destination + " : " + departureTime);
     }
     
