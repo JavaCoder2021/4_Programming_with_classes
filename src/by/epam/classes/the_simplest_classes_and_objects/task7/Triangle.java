2f33aec7f1bf329513ad6c7bfea35ee425d1ac5f
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-12-12 19:37:49 +0200

    Update Triangle.java

--- src/by/epam/classes/the_simplest_classes_and_objects/task7/Triangle.java
+++ src/by/epam/classes/the_simplest_classes_and_objects/task7/Triangle.java
@@ -1,27 +1,25 @@
 /*
-7.  Описать  класс,  представляющий  треугольник.  
-Предусмотреть  методы  для  
-- создания объектов,  
-- вычисления площади, 
-- периметра и 
-- точки пересечения медиан.  
-*/
-
-package by.epam.classes.the_simplest_classes_and_objects;
+ * 7.
+ * Описать  класс,  представляющий  треугольник.  
+ * Предусмотреть  методы  для  
+ * - создания объектов,  
+ * - вычисления площади, 
+ * - периметра и 
+ * - точки пересечения медиан.  
+ */
+package by.epam.classes.the_simplest_classes_and_objects.task7;
 
 public class Triangle {
     
     private Point a, b, c;
     
-    public Triangle(Point a_, Point b_, Point c_) 
-    {
+    public Triangle(Point a_, Point b_, Point c_) {
         a = a_;
         b = b_;
         c = c_;
     }
     
-    public double square() 
-    {
+    public double square() {
         double ab = Point.distance(a, b);
         double bc = Point.distance(b, c);
         double ac = Point.distance(a, c);
@@ -31,8 +29,7 @@ public class Triangle {
         return Math.sqrt(p * (p - ab) * (p - bc) * (p - ac));
     }
     
-    public double perimeter() 
-    {
+    public double perimeter() {
         double ab = Point.distance(a, b);
         double bc = Point.distance(b, c);
         double ac = Point.distance(a, c);
@@ -40,8 +37,7 @@ public class Triangle {
         return ab + bc + ac;
     }
     
-    public Point median_crossing() 
-    {
+    public Point median_crossing() {
         return new Point((a.getX() + b.getX() + c.getX()) / 3, (a.getY() + b.getY() + c.getY()) / 3);
     }
     
