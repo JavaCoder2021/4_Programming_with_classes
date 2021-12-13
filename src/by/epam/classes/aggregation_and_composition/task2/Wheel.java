8dc137287aa48e72e80f1d19a1ca45410d140890
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-12-13 20:34:28 +0200

    Update Wheel.java

--- src/by/epam/classes/aggregation_and_composition/task2/Wheel.java
+++ src/by/epam/classes/aggregation_and_composition/task2/Wheel.java
@@ -1,4 +1,4 @@
-package by.epam.classes.aggregation_and_composition;
+package by.epam.classes.aggregation_and_composition.task2;
 
 import java.util.*;
 
@@ -9,30 +9,25 @@ class Wheel {
     private double diameter;
     private Type type;
   
-    public Wheel(double diameter, Type type) 
-    {
+    public Wheel(double diameter, Type type) {
         this.diameter = diameter;
         this.type = type;
     }
   
-    public static Wheel get(Scanner scanner) 
-    {
+    public static Wheel get(Scanner scanner) {
         Wheel object = new Wheel(0, Type.Winter);
        
         System.out.print("Diameter: ");
         object.diameter = scanner.nextDouble();
     
         System.out.print("Type (1) - Winter, (2) - Summer: ");
-        while (true) 
-        {
+        while (true) {
             int value = scanner.nextInt();
-            if (value == 1) 
-            {
+            if (value == 1) {
                 object.type = Type.Winter;
                 break;
             }
-            if (value == 2) 
-            {
+            if (value == 2) {
                 object.type = Type.Summer;
                 break;
             }
@@ -43,14 +38,12 @@ class Wheel {
         return object;
     }
   
-    public double getDiameter() 
-    {
+    public double getDiameter() {
         return diameter;
     }
     
     @Override
-    public String toString() 
-    {     
+    public String toString() {     
         return "d = " + diameter + "; " + (type == Type.Winter ? "Winter" : "Summer");
     }
     
