237aff77ee344fc300bd85532c3aa77ba45920e6
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-12-13 21:38:24 +0200

    Update BankAccount.java

--- src/by/epam/classes/aggregation_and_composition/task4/BankAccount.java
+++ src/by/epam/classes/aggregation_and_composition/task4/BankAccount.java
@@ -1,72 +1,54 @@
-/*
-4. 
-Счета. Клиент может иметь несколько счетов в банке. 
-- Учитывать возможность блокировки/разблокировки счета. 
-- Реализовать поиск и сортировку счетов. 
-- Вычисление общей суммы по счетам. 
-- Вычисление суммы по всем счетам, имеющим положительный и отрицательный балансы отдельно. 
-*/
+package by.epam.classes.aggregation_and_composition.task4;
 
-package by.epam.classes.aggregation_and_composition;
 import java.util.Comparator;
 
 public class BankAccount {
     
     private int id = 0;
     private static int  ID          = 0;
-    private String      bankAccount = "00000000";
+    private String      bankAccount;
     private boolean     blocked;
     private int         sum         = 0;
     
-    public BankAccount(int sum)
-    {
-        id = ID++;
-        String str = Integer.toString(id);
-        bankAccount = bankAccount.substring(0, bankAccount.length() - str.length()) + str;        
+    public BankAccount(int sum) {
+        id = ++ID;
+        bankAccount = String.format("%08d", id);        
         blocked = false;
         this.sum = sum;
     }
     
-    public int getId()
-    {
+    public int getId() {
         return id;
     }
     
-    public int getSum()
-    {
+    public int getSum() {
         return sum;
     }    
     
-    public void toBlockAccount()
-    {
+    public void toBlockAccount() {
         blocked = true;
     }
     
-    public void toUnblockAccount()
-    {
+    public void toUnblockAccount() {
         blocked = false;
     }
     
-    public static BankAccount createBankAccount(int sum)
-    {        
+    public static BankAccount createBankAccount(int sum) {        
         BankAccount bankAccount = new BankAccount(sum);
         
         return bankAccount;
     }
     
-    public static class ByNumberComparator implements Comparator < BankAccount > 
-    {
+    public static class ByNumberComparator implements Comparator < BankAccount > {
         @Override
-        public int compare(BankAccount left, BankAccount right) 
-        {
+        public int compare(BankAccount left, BankAccount right) {
             return right.sum - left.sum;
         }
     }
     
     @Override
-    public String toString()
-    {
-        return "Id = " + id + "; Bank account № " + bankAccount + "; sum = " + sum + "; " + "blocked = " + blocked;
+    public String toString(){
+        return "Id = " + id + "; Bank account №" + bankAccount + "; sum = " + sum + "; " + "blocked = " + blocked;
     }
     
 }
