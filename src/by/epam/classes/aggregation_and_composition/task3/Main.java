5652a2fa99c51b0a9efbf4543f2a8b4d2ded5d84
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-12-13 20:46:26 +0200

    Create Main.java

---
+++ src/by/epam/classes/aggregation_and_composition/task3/Main.java
@@ -0,0 +1,81 @@
+/*
+ * 3. 
+ * Создать объект класса Государство, используя классы Область, Район, Город. 
+ * Методы: вывести на консоль столицу, количество областей, площадь, областные центры.  
+ */
+package by.epam.classes.aggregation_and_composition.task3;
+
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Country country = new Country("Belarus", "Minsk");
+        int choice;
+        
+        while (true) {
+            menuText();
+            choice = scannerInt();
+            if (choice == 0)
+                break;
+            if (choice < 0 || choice > 5) {
+                System.out.println("Incorrect choose!");
+                continue;
+            }
+                      
+            switch (choice) {
+                case 1:
+                    country.addRegion();
+                    break;           
+                case 2:
+                    System.out.println("Capital: " + country.getCapital());
+                    break;           
+                case 3:
+                    System.out.println("Number of regions: " + country.getRegionList().size());
+                    break; 
+                case 4:
+                    System.out.println("Area of all regions: " + country.squareRegions());
+                    break;
+                case 5:
+                    System.out.println("Regional cities: ");
+                    country.regionalCities();
+                    break;
+            }     
+            
+        } 
+        
+    }
+    
+    private static int scannerInt() {
+        
+        Scanner input = new Scanner(System.in);
+        int num = -1;
+        
+        do {
+            if (input.hasNextInt()) {
+                num = input.nextInt();
+            } 
+            else {
+                input.next();
+            }
+        } while (num < 0);
+
+        return num;
+        
+    }
+    
+    private static void menuText() { 
+        System.out.print(
+            "\n" + 
+            "0 - Exit\n" +
+            "1 - Add area\n" +
+            "2 - Display the capital\n" +
+            "3 - Number of areas\n" +
+            "4 - Area\n" +
+            "5 - Regional centers\n" +                   
+            "\n"
+        );   
+    } 
+    
+}
