8933494d315831928d5b0be33754950a31583aaf
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-01-30 13:38:10 +0200

    Update Test1.java

--- src/by/epam/classes/the_simplest_classes_and_objects/task1/Test1.java
+++ src/by/epam/classes/the_simplest_classes_and_objects/task1/Test1.java
@@ -1,47 +1,49 @@
 /*
  * 1.  
- * - Создайте  класс  Test1  двумя  переменными.  
- * - Добавьте  метод  вывода  на  экран  и  методы  изменения  этих переменных. 
- * - Добавьте метод,  который  находит  сумму  значений  этих  переменных,  
- * - и  метод,  который  находит наибольшее значение из этих двух переменных. 
+ * Создайте  класс  Test1  двумя  переменными.  
+ * Добавьте  метод  вывода  на  экран  и  методы  изменения  этих переменных. 
+ * Добавьте метод,  который  находит  сумму  значений  этих  переменных,  
+ * и  метод,  который  находит наибольшее значение из этих двух переменных. 
  */
 package by.epam.classes.the_simplest_classes_and_objects.task1;
 
 public class Test1 {
-    
-    private int a;
-    private int b;
-    private int sum;
-    private int max;
-       
-    public void setA(int a) {
-        this.a = a;
-    }
-    
-    public void setB(int b) {
-        this.b = b;
-    }
-    
-    public void displayA() {
-        System.out.println("a = " + a);
-    }
-    
-    public void displayB() {
-        System.out.println("b = " + b);
-    }
-    
-    public void sumAB() {
-        sum = a + b;
-        System.out.println("sum = " + sum);
-    }
-    
-    public void maxAB() {
-        if (a >= b)
-            max = a;
-        else
-            max = b;
 
-        System.out.println("max = " + max);
-    }
-    
+	private int a;
+	private int b;
+
+	public Test1() {
+		super();
+	}
+
+	public Test1(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	public int sumAB() {
+		return (a + b);
+	}
+
+	public int maxAB() {
+		return (a >= b) ? a : b;
+	}
+
 }
