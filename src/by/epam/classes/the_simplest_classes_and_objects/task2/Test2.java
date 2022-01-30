4631f654efb336f01941504ee706040fbbb83ce9
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-01-30 13:44:31 +0200

    Update Test2.java

--- src/by/epam/classes/the_simplest_classes_and_objects/task2/Test2.java
+++ src/by/epam/classes/the_simplest_classes_and_objects/task2/Test2.java
@@ -1,9 +1,9 @@
 /*
  * 2.  
- * - Создайте  класс  Test2  двумя  переменными.  
- * - Добавьте  конструктор  с  входными  параметрами.  
- * - Добавьте конструктор, инициализирующий члены класса по умолчанию. 
- * - Добавьте set- и get- методы для полей экземпляра класса.
+ * Создайте  класс  Test2  двумя  переменными.  
+ * Добавьте  конструктор  с  входными  параметрами.  
+ * Добавьте конструктор, инициализирующий члены класса по умолчанию. 
+ * Добавьте set- и get- методы для полей экземпляра класса.
  */
 package by.epam.classes.the_simplest_classes_and_objects.task2;
 
@@ -12,30 +12,30 @@ public class Test2 {
     private int a;
     private int b;
     
-    public Test2(int a, int b) {
-        this.a = a;
-        this.b = b;
-    }
-    
     public Test2() {
-        a = 5;
-        b = 10;
+		super();
     }
     
-    public void setA(int a) {
-        this.a = a;
-    }
-    
-    public void setB(int b) {
-        this.b = b;
-    }
-    
-    public int getA() {
-        return a;
-    }
-    
-    public int getB() {
-        return b;
-    }
+    public Test2(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
            
 }
