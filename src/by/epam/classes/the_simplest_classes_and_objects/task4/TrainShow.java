40f414738046d27457d73cc170cc9af5400a8535
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-03-06 15:55:50 +0200

    Create TrainShow.java

---
+++ src/by/epam/classes/the_simplest_classes_and_objects/task4/TrainShow.java
@@ -0,0 +1,18 @@
+package by.epam.classes.the_simplest_classes_and_objects.task4;
+
+public class TrainShow {
+	
+	public static void allTrains(Train[] trains) {
+
+		for (Train train : trains)
+			System.out.println(train);
+
+	}
+
+	public static void oneTrain(Train train) {
+
+		System.out.println(train);
+
+	}
+
+}
