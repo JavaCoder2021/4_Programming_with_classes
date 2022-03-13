bef308b8f6ca6ffb15e5f3d59b7dacc3900918ff
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-03-13 11:52:06 +0200

    Update Point.java

--- src/by/epam/classes/the_simplest_classes_and_objects/task7/Point.java
+++ src/by/epam/classes/the_simplest_classes_and_objects/task7/Point.java
@@ -1,30 +1,30 @@
 package by.epam.classes.the_simplest_classes_and_objects.task7;
 
 public class Point {
-    
-    private double x, y;
-    
-    public Point() {
+
+	private double x, y;
+
+	public Point() {
 		super();
-	}     
-    
-    public Point(double x, double y) {
+	}
+
+	public Point(double x, double y) {
 		super();
 		this.x = x;
 		this.y = y;
 	}
-    
-    public double getX() {
-        return x;
-    }
-    
-    public double getY() {
-        return y;
-    }
-    
-    @Override
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	@Override
 	public String toString() {
 		return getClass().getSimpleName() + " [x=" + x + ", y=" + y + "]";
 	}
-    
+
 }
