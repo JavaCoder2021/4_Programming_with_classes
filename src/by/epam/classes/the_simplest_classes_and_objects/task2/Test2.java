97334b8a568a8cf25043181c0138894f87af1384
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-03-13 14:38:44 +0200

    Update Test2.java

--- src/by/epam/classes/the_simplest_classes_and_objects/task2/Test2.java
+++ src/by/epam/classes/the_simplest_classes_and_objects/task2/Test2.java
@@ -38,4 +38,34 @@ public class Test2 {
 		this.b = b;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + a;
+		result = prime * result + b;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Test2 other = (Test2) obj;
+		if (a != other.a)
+			return false;
+		if (b != other.b)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + " [a=" + a + ", b=" + b + "]";
+	}
+
 }
