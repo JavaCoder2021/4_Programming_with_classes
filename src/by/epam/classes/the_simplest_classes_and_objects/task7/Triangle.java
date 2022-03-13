b7341f2a01597c34ef736f93f84a79eb2b860639
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-03-13 11:57:36 +0200

    Update Triangle.java

--- src/by/epam/classes/the_simplest_classes_and_objects/task7/Triangle.java
+++ src/by/epam/classes/the_simplest_classes_and_objects/task7/Triangle.java
@@ -10,44 +10,52 @@
 package by.epam.classes.the_simplest_classes_and_objects.task7;
 
 public class Triangle {
-    
-    private Point a, b, c;
-    
-    public Triangle(Point a_, Point b_, Point c_) {
-        a = a_;
-        b = b_;
-        c = c_;
-    }
-    
-    public double square() {
-        double ab = Point.distance(a, b);
-        double bc = Point.distance(b, c);
-        double ac = Point.distance(a, c);
-        
-        double p = (ab + bc + ac) / 2;
-        
-        return Math.sqrt(p * (p - ab) * (p - bc) * (p - ac));
-    }
-    
-    public double perimeter() {
-        double ab = Point.distance(a, b);
-        double bc = Point.distance(b, c);
-        double ac = Point.distance(a, c);
-        
-        return ab + bc + ac;
-    }
-    
-    public Point median_crossing() {
-        return new Point((a.getX() + b.getX() + c.getX()) / 3, (a.getY() + b.getY() + c.getY()) / 3);
-    }
-    
-    public void print() {
-        System.out.print("A: ");
-        a.print();
-        System.out.print("B: ");
-        b.print();
-        System.out.print("C: ");
-        c.print();
-    }
-    
+
+	private Point a, b, c;
+
+	public Triangle() {
+		super();
+	}
+
+	public Triangle(Point a, Point b, Point c) {
+		super();
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	public double distance(Point a, Point b) {
+		double dx = a.getX() - b.getX();
+		double dy = a.getY() - b.getY();
+
+		return Math.sqrt(dx * dx + dy * dy);
+	}
+
+	public double square() {
+		double ab = distance(a, b);
+		double bc = distance(b, c);
+		double ac = distance(a, c);
+
+		double p = (ab + bc + ac) / 2;
+
+		return Math.sqrt(p * (p - ab) * (p - bc) * (p - ac));
+	}
+
+	public double perimeter() {
+		double ab = distance(a, b);
+		double bc = distance(b, c);
+		double ac = distance(a, c);
+
+		return ab + bc + ac;
+	}
+
+	public Point medianCrossing() {
+		return new Point((a.getX() + b.getX() + c.getX()) / 3, (a.getY() + b.getY() + c.getY()) / 3);
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + " [a=" + a + ", b=" + b + ", c=" + c + "]";
+	}
+
 }
