b44595327868ef0373a5dc154b4180dbbb90a82d
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-03-13 14:23:08 +0200

    Update Shelf.java

--- src/by/epam/classes/the_simplest_classes_and_objects/task9/Shelf.java
+++ src/by/epam/classes/the_simplest_classes_and_objects/task9/Shelf.java
@@ -39,4 +39,34 @@ public class Shelf {
 		books.removeAll(books);
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((books == null) ? 0 : books.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Shelf other = (Shelf) obj;
+		if (books == null) {
+			if (other.books != null)
+				return false;
+		} else if (!books.equals(other.books))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Shelf [books=" + books + "]";
+	}
+
 }
