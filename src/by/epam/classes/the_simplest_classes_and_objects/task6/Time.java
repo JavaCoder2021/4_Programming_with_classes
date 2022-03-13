8d22c410f882258bb0d1db4d74c5569929ccadd8
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-03-13 14:34:38 +0200

    Update Time.java

--- src/by/epam/classes/the_simplest_classes_and_objects/task6/Time.java
+++ src/by/epam/classes/the_simplest_classes_and_objects/task6/Time.java
@@ -65,6 +65,34 @@ public class Time {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + hours;
+		result = prime * result + min;
+		result = prime * result + sec;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		if (hours != other.hours)
+			return false;
+		if (min != other.min)
+			return false;
+		if (sec != other.sec)
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return hours + " ч " + min + " мин " + sec + " сек";
 	}
