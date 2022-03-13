481b10f3c27593741abee9fdda53c0e61a64b6aa
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-03-13 12:15:59 +0200

    Update Main.java

--- src/by/epam/classes/the_simplest_classes_and_objects/task7/Main.java
+++ src/by/epam/classes/the_simplest_classes_and_objects/task7/Main.java
@@ -12,32 +12,54 @@ package by.epam.classes.the_simplest_classes_and_objects.task7;
 import java.util.Scanner;
 
 public class Main {
-    
-    public static void main(String[] args) {
-        
-        Scanner scanner = new Scanner(System.in);
-        
-        Point a = new Point();
-        Point b = new Point();
-        Point c = new Point();
-        
-        System.out.println("Enter point A: ");
-        a.read(scanner);
-        System.out.println("Enter point B: ");
-        b.read(scanner);
-        System.out.println("Enter point C: ");
-        c.read(scanner);
-               
-        Triangle triangle = new Triangle(a, b, c);
-        triangle.print();
-        
-        System.out.println("Square: " + triangle.square());
-        
-        System.out.println("Perimeter: " + triangle.perimeter());
-        
-        System.out.print("medianCrossing: ");
-        triangle.median_crossing().print();
-        
-    }
-    
+
+	public static void main(String[] args) {
+
+		Point a = new Point();
+		Point b = new Point();
+		Point c = new Point();
+
+		System.out.println("Enter point A: ");
+		a.setX(getDoubleFromConsol("X = "));
+		a.setY(getDoubleFromConsol("Y = "));
+		
+		System.out.println("Enter point B: ");
+		b.setX(getDoubleFromConsol("X = "));
+		b.setY(getDoubleFromConsol("Y = "));
+		
+		System.out.println("Enter point C: ");
+		c.setX(getDoubleFromConsol("X = "));
+		c.setY(getDoubleFromConsol("Y = "));
+
+		Triangle triangle = new Triangle(a, b, c);
+		System.out.println(triangle);
+
+		System.out.println("Square: " + triangle.square());
+
+		System.out.println("Perimeter: " + triangle.perimeter());
+
+		System.out.print("medianCrossing: ");
+		System.out.println(triangle.medianCrossing());
+
+	}
+
+	public static double getDoubleFromConsol(String str) {
+
+		double a;
+
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print(str);
+		while (!scanner.hasNextDouble()) {
+			scanner.next();
+			System.out.println("Invalid input!");
+			System.out.print(str);
+		}
+		a = scanner.nextDouble();
+
+		return a;
+
+	}
+
 }
