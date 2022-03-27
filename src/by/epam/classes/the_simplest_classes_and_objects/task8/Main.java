fd56f6dcc3cbf584ed8f7a534ce964044f16a69a
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-03-27 21:59:00 +0200

    Update Main.java

--- src/by/epam/classes/the_simplest_classes_and_objects/task8/Main.java
+++ src/by/epam/classes/the_simplest_classes_and_objects/task8/Main.java
@@ -14,74 +14,74 @@ package by.epam.classes.the_simplest_classes_and_objects.task8;
 import java.util.*;
 
 public class Main {
-    
-    public static void main(String[] args) {
-        
-        Customers customers = new Customers();
-    
-        Scanner input = new Scanner(System.in);
-        int choice;
-        
-        while (true) {
-            menuText();
-            choice = scannerInt();
-            if (choice == 0)
-                break;
-            if (choice < 0 || choice > 3) {
-                System.out.println("Incorrect choose!");
-                continue;
-            }
-                      
-            switch (choice) {
-                case 1:
-                    customers.addCustomer(input);
-                    break;           
-                case 2:
-                    customers.sortByName();
-                    customers.print();
-                    break;           
-                case 3:
-                    System.out.println("Enter your credit card range\n");
-                    System.out.println("From: ");
-                    String from = input.nextLine();
-                    System.out.println("Before: ");
-                    String before = input.nextLine();
-                    customers.sortCards(from, before);
-                break;             
-            }     
-            
-        }    
-        
-    }
-    
-    private static int scannerInt() {
-        
-        Scanner input = new Scanner(System.in);
-        int num = -1;
-        
-        do {
-            if (input.hasNextInt()) {
-                num = input.nextInt();
-            } 
-            else {
-                input.next();
-            }
-        } while (num < 0);
 
-        return num;
-        
-    }
-    
-    private static void menuText() { 
+	public static void main(String[] args) {
+
+		int choice;
+    	List<Customer> sortedCustomers;
+		Customers customers = new Customers();
+		CustomerView customerView = new CustomerView();
+		CustomerLogic customerLogic = new CustomerLogic();
+		
+		customers.addCustomer(new Customer("Ivanov", "Ivan", "Ivanovich", "Moscow", 123456));
+		customers.addCustomer(new Customer("Sidorov", "Sidr", "Sidorovich", "Minsk", 654321));
+		customers.addCustomer(new Customer("Petrov", "Petr", "Petrovich", "Saint Petersburg", 333333));
+
+		while (true) {
+			menuText();
+			choice = getIntFromConsol("");
+			if (choice < 0 || choice > 2) {
+				System.out.println("Incorrect choose!");
+				continue;
+			}
+
+			if (choice == 0)
+				break;
+			switch (choice) {
+			case 1:
+				Collections.sort(customers.getCustomers(), customerLogic.new BySurnameComparator());
+				customerView.print(customers.getCustomers());
+				break;
+			case 2:
+				System.out.println("Enter your credit card range\n");
+				int from = getIntFromConsol("From: ");
+				int to = getIntFromConsol("To: ");
+				sortedCustomers = customerLogic.ByCustomersComparator(from, to, customers.getCustomers());
+				customerView.print(sortedCustomers);
+				break;
+			}
+
+		}
+
+	}
+	
+    public static void menuText() { 
         System.out.print(
             "\n" + 
             "Select the menu item:\n" +
             "0. Exit\n" +
-            "1. Add customer\n" +
-            "2. Display of buyers in alphabetical order\n" +
-            "3. Display of buyers with a credit card in the interval\n" +
+            "1. Display of customers in alphabetical order\n" +
+            "2. Display of customers with a credit card in the interval\n" +
             "\n"
         );   
     }
-    
+
+	public static int getIntFromConsol(String str) {
+
+		int a;
+
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print(str);
+		while (!scanner.hasNextInt()) {
+			scanner.nextLine();
+			System.out.print(str);
+		}
+		a = scanner.nextInt();
+
+		return a;
+
+	}
+
 }
