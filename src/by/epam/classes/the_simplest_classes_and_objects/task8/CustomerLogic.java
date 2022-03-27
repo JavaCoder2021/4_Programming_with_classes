f8f8d1fd5e1f13bdc72e930056932a43ea82b534
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-03-27 22:00:30 +0200

    Create CustomerLogic.java

---
+++ src/by/epam/classes/the_simplest_classes_and_objects/task8/CustomerLogic.java
@@ -0,0 +1,34 @@
+package by.epam.classes.the_simplest_classes_and_objects.task8;
+
+import java.util.*;
+
+public class CustomerLogic {
+
+	public class BySurnameComparator implements Comparator<Customer> {	
+
+		@Override
+		public int compare(Customer left, Customer right) {
+			if (left.getSurname() != right.getSurname())
+				return left.getSurname().compareTo(right.getSurname());
+			if (left.getName() != right.getName())
+				return left.getName().compareTo(right.getName());
+			return left.getPatronymic().compareTo(right.getPatronymic());
+		}
+
+	}
+
+	public List<Customer> ByCustomersComparator(int from, int to, List<Customer> customers) {
+
+		List<Customer> sortedCustomers = new LinkedList<Customer>();
+
+		for (Customer nowCustomer : customers) {
+			if (nowCustomer.getCreditCardNumber() >= from
+					&& nowCustomer.getCreditCardNumber() <= to)
+				sortedCustomers.add(nowCustomer);
+		}
+
+		return sortedCustomers;
+
+	}
+
+}
