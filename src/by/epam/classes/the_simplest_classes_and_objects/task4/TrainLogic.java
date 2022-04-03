fbdba5582834b3ccb67e470bc595b07a3921e10b
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-04-03 16:46:50 +0200

    Update TrainLogic.java

--- src/by/epam/classes/the_simplest_classes_and_objects/task4/TrainLogic.java
+++ src/by/epam/classes/the_simplest_classes_and_objects/task4/TrainLogic.java
@@ -1,42 +1,50 @@
 package by.epam.classes.the_simplest_classes_and_objects.task4;
 
 import java.util.Comparator;
+import java.util.Collections;
 import java.util.List;
 
 public class TrainLogic {
 
-	public class ByNumberComparator implements Comparator<Train> {
+    public void sortByNumberComparator(List<Train> trains) {
 
-		@Override
-		public int compare(Train left, Train right) {
-			return left.getTrainNumber() - right.getTrainNumber();
-		}
+        Collections.sort(trains, new Comparator<Train>() {
+            @Override
+            public int compare(Train left, Train right) {
+                return left.getTrainNumber() - right.getTrainNumber();
+            }
+        });
+        
+    }
 
-	}
+    public void sortByDestinationAndTimeComparator(List<Train> trains) {
 
-	public class ByDestinationAndTimeComparator implements Comparator<Train> {
+        Collections.sort(trains, new Comparator<Train>() {
+            @Override
+            public int compare(Train left, Train right) {
+                if (left.getDestination().equals(right.getDestination())) {
+                    return left.getDepartureTime().compareTo(right.getDepartureTime());
+                }
+                return left.getDestination().compareTo(right.getDestination());
+            }
 
-		@Override
-		public int compare(Train left, Train right) {
-			if (left.getDestination() == right.getDestination())
-				return left.getDepartureTime().compareTo(right.getDepartureTime());
-			return left.getDestination().compareTo(right.getDestination());
-		}
+        });
 
-	}
+    }
 
-	public Train findByNumber(int nowTrainNumber, List<Train> trains) {
-		
-		Train train = null;
+    public Train sortByNumber(int nowTrainNumber, List<Train> trains) {
 
-		for (Train nowTrain : trains)
-			if (nowTrainNumber == nowTrain.getTrainNumber()) {
-				train = nowTrain;
-				break;
-			}
+        Train train = null;
 
-		return train;
+        for (Train nowTrain : trains) {
+            if (nowTrainNumber == nowTrain.getTrainNumber()) {
+                train = nowTrain;
+                break;
+            }
+        }
 
-	}
+        return train;
+
+    }
 
 }
