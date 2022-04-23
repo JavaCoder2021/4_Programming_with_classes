f631f83aff8baabaac159a3662ee18420de96864
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-04-23 21:11:49 +0200

    Update Main.java

--- src/by/epam/classes/the_simplest_classes_and_objects/task10/Main.java
+++ src/by/epam/classes/the_simplest_classes_and_objects/task10/Main.java
@@ -16,59 +16,60 @@ package by.epam.classes.the_simplest_classes_and_objects.task10;
 import java.util.*;
 
 public class Main {
-    
-    public static void main(String[] args) {
-        
-        Airlines airlines = new Airlines();
-    
-        Scanner input = new Scanner(System.in);
-        int choice;
-        
-        while (true) {
-            menuText();
-            choice = scannerInt();
-            if (choice == 0)
-                break;
-            if (choice < 0 || choice > 3) {
-                System.out.println("Incorrect choose!");
-                continue;
-            }
-                      
-            switch (choice) {
-                case 1:
-                    airlines.sortByDestination(input);
-                    break;           
-                case 2:
-                    airlines.sortByDaysOfWeek(input);
-                    break;           
-                case 3:
-                    airlines.sortByDepartureTime(input);
-                    break;             
-            }     
-            
-        }    
-        
-    }
-    
-    private static int scannerInt() {
-        
-        Scanner input = new Scanner(System.in);
-        int num = -1;
-        
-        do {
-            if (input.hasNextInt()) {
-                num = input.nextInt();
-            } 
-            else {
-                input.next();
-            }
-        } while (num < 0);
 
-        return num;
-        
-    }
-    
-    private static void menuText() { 
+	public static void main(String[] args) {
+
+		Airlines airlines = new Airlines();
+		AirlineView airlineView = new AirlineView();
+		AirlineLogic airlineLogic = new AirlineLogic();
+		List<Airline> sortedAirlines;
+		int choice;
+		String destination, daysOfWeek;
+		double departureTime;
+
+		airlines.addAirline(new Airline(105, "Moscow", "Boing", "13.55", "Monday"));
+		airlines.addAirline(new Airline(35, "Minsk", "Boing", "10.05", "Saturday"));
+		airlines.addAirline(new Airline(125, "Kiev", "Boing", "09.20", "Sunday"));
+		airlines.addAirline(new Airline(55, "Moscow", "Boing", "21.45", "Sunday"));
+		airlines.addAirline(new Airline(45, "Minsk", "Boing", "19.15", "Wednesday"));
+
+		while (true) {
+
+			menuText();
+			choice = getIntFromConsol("");
+
+			if (choice < 0 || choice > 3) {
+				System.out.println("Incorrect choose!");
+				continue;
+			}
+
+			if (choice == 0)
+				break;
+			switch (choice) {
+			case 1:
+		        destination = getStringFromConsol("Destination: ");
+		        sortedAirlines = airlineLogic.sortByDestination(airlines.getAirlines(), destination);
+		        airlineView.print(sortedAirlines);
+				break;
+			case 2:
+		        daysOfWeek = getStringFromConsol("Day of week: ");
+		        sortedAirlines = airlineLogic.sortByDaysOfWeek(airlines.getAirlines(), daysOfWeek);
+		        airlineView.print(sortedAirlines);
+				break;
+			case 3:
+		        daysOfWeek = getStringFromConsol("Day of week: ");
+		        departureTime = getDoubleFromConsol("Departure time: ");
+		        sortedAirlines = airlineLogic.sortByDepartureTime(airlines.getAirlines(), daysOfWeek, departureTime);
+		        airlineView.print(sortedAirlines);
+				break;
+			}
+
+		}
+
+	}
+
+	public static void menuText() {
+		
         System.out.print(
             "\n" + 
             "Select the menu item:\n" +
@@ -77,7 +78,56 @@ public class Main {
             "2. List of flights for a given day of the week\n" +
             "3. List of flights for a given day of the week, the departure time for which is longer than the specified one\n" +
             "\n"
-        );   
+        );
+        
     }
-    
+
+	public static String getStringFromConsol(String str) {
+
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print(str);
+		String line = scanner.nextLine();
+
+		return line;
+
+	}
+
+	public static int getIntFromConsol(String str) {
+
+		int a;
+
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print(str);
+		while (!scanner.hasNextInt()) {
+			scanner.nextLine();
+			System.out.print(str);
+		}
+		a = scanner.nextInt();
+
+		return a;
+
+	}
+	
+	public static double getDoubleFromConsol(String str) {
+
+		double a;
+
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print(str);
+		while (!scanner.hasNextDouble()) {
+			scanner.nextLine();
+			System.out.print(str);
+		}
+		a = scanner.nextDouble();
+
+		return a;
+
+	}
+
 }
