d7bb5b67ca8828e7dba3c68804bef3f5628db8b5
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-04-23 21:13:53 +0200

    Update Airline.java

--- src/by/epam/classes/the_simplest_classes_and_objects/task10/Airline.java
+++ src/by/epam/classes/the_simplest_classes_and_objects/task10/Airline.java
@@ -14,65 +14,112 @@
 package by.epam.classes.the_simplest_classes_and_objects.task10;
 
 public class Airline {
-    
-    private int number;
-    private String destination, aircraftType, departureTime, daysOfWeek;
-    
-    public Airline(String destination, int number, String aircraftType, String departureTime, String daysOfWeek) {
-        this.destination    = destination;
-        this.number         = number;  
-        this.aircraftType   = aircraftType;
-        this.departureTime  = departureTime;
-        this.daysOfWeek     = daysOfWeek;        
-    }
-    
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }    
-    
-    public void setNumber(int number) {
-        this.number = number;
-    }  
-    
-    public void setAircraftType(String aircraftType) {
-        this.aircraftType = aircraftType;
-    }
-    
-    public void setDepartureTime(String departureTime) {
-        this.departureTime = departureTime;
-    }
-    
-    public void setDaysOfWeek(String daysOfWeek) {
-        this.daysOfWeek = daysOfWeek;
-    }
-    
-    public String getDestination() {
-        return destination;
-    }
-    
-    public int getNumber() {
-        return number;
-    }
-    
-    public String getAircraftType() {
-        return aircraftType;
-    }
-    
-    public String getDepartureTime() {
-        return departureTime;
-    }
- 
-    public String getDaysOfWeek() {
-        return daysOfWeek;
-    }    
-    
-    @Override
-    public String toString() {
-        return number + ". " + aircraftType + " --> " + destination + " - " + daysOfWeek + " - " + departureTime;
-    }
-    
-    public void print() {
-        System.out.println(toString());
-    }   
-    
+
+	private int number;
+	private String destination, aircraftType, departureTime, daysOfWeek;
+
+	public Airline() {
+		super();
+	}
+
+	public Airline(int number, String destination, String aircraftType, String departureTime, String daysOfWeek) {
+		super();
+		this.number = number;
+		this.destination = destination;
+		this.aircraftType = aircraftType;
+		this.departureTime = departureTime;
+		this.daysOfWeek = daysOfWeek;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public String getAircraftType() {
+		return aircraftType;
+	}
+
+	public void setAircraftType(String aircraftType) {
+		this.aircraftType = aircraftType;
+	}
+
+	public String getDepartureTime() {
+		return departureTime;
+	}
+
+	public void setDepartureTime(String departureTime) {
+		this.departureTime = departureTime;
+	}
+
+	public String getDaysOfWeek() {
+		return daysOfWeek;
+	}
+
+	public void setDaysOfWeek(String daysOfWeek) {
+		this.daysOfWeek = daysOfWeek;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((aircraftType == null) ? 0 : aircraftType.hashCode());
+		result = prime * result + ((daysOfWeek == null) ? 0 : daysOfWeek.hashCode());
+		result = prime * result + ((departureTime == null) ? 0 : departureTime.hashCode());
+		result = prime * result + ((destination == null) ? 0 : destination.hashCode());
+		result = prime * result + number;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Airline other = (Airline) obj;
+		if (aircraftType == null) {
+			if (other.aircraftType != null)
+				return false;
+		} else if (!aircraftType.equals(other.aircraftType))
+			return false;
+		if (daysOfWeek == null) {
+			if (other.daysOfWeek != null)
+				return false;
+		} else if (!daysOfWeek.equals(other.daysOfWeek))
+			return false;
+		if (departureTime == null) {
+			if (other.departureTime != null)
+				return false;
+		} else if (!departureTime.equals(other.departureTime))
+			return false;
+		if (destination == null) {
+			if (other.destination != null)
+				return false;
+		} else if (!destination.equals(other.destination))
+			return false;
+		if (number != other.number)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return number + ". " + aircraftType + " --> " + destination + " - " + daysOfWeek + " - " + departureTime;
+	}
+
 }
