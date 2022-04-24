f012fd7ae1ba76a2df6539d86e367ee4dff5545c
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-04-24 13:44:44 +0200

    Update Sentence.java

--- src/by/epam/classes/aggregation_and_composition/task1/Sentence.java
+++ src/by/epam/classes/aggregation_and_composition/task1/Sentence.java
@@ -3,28 +3,74 @@ package by.epam.classes.aggregation_and_composition.task1;
 import java.util.*;
 
 public class Sentence {
-    
-    private ArrayList<String> words = new ArrayList<>();
 
-    public Sentence() {
-        //
-    }
-    
-    public Sentence(Word word) {
-        addWord(word);
-    }
-        
-    public void addWord(Word word) {
-        this.words.add(word.getValue());
-    }
-    
-    public void print() {
-        for (int i = 0; i < words.size(); i++) {
-            if ((i + 1) == words.size())
-                System.out.print(words.get(i) + ". ");
-            else
-                System.out.print(words.get(i) + " ");
-        }
-    }
-    
+	private String sentence = "";
+	private List<Word> words = new ArrayList<Word>();
+
+	public Sentence() {
+		super();
+	}
+
+	public Sentence(Word word) {
+		super();
+		words.add(word);
+	}
+
+	public Sentence(List<Word> words) {
+		super();
+		this.words = words;
+	}
+
+	public void addWord(Word word) {
+		this.words.add(word);
+	}
+
+	public void removeWord(Word word) {
+		this.words.remove(word);
+	}
+
+	public void addWords(List<Word> words) {
+		this.words.addAll(words);
+	}
+
+	public List<Word> getWords() {
+		return words;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((words == null) ? 0 : words.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sentence other = (Sentence) obj;
+		if (words == null) {
+			if (other.words != null)
+				return false;
+		} else if (!words.equals(other.words))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		for (int i = 0; i < words.size(); i++) {
+			if ((i + 1) == words.size())
+				sentence = sentence + words.get(i) + ".";
+			else
+				sentence = sentence + words.get(i) + " ";
+		}
+		return sentence;
+	}
+
 }
