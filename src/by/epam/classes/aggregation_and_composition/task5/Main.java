9ff3b59ec9565645d13cd8d5f52984f67a0d318a
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-05-03 10:32:37 +0200

    Update Main.java

--- src/by/epam/classes/aggregation_and_composition/task5/Main.java
+++ src/by/epam/classes/aggregation_and_composition/task5/Main.java
@@ -4,74 +4,92 @@
  * Сформировать набор предложений клиенту по выбору туристической путевки различного типа 
  * (отдых, экскурсии, лечение, шопинг, круиз и т. д.) для оптимального выбора. 
  * Учитывать возможность выбора транспорта, питания и числа дней. 
- * Реализовать выбор и сортировку путевок. 
+ * Реализовать выбор и сортировку путевок.
  */
 package by.epam.classes.aggregation_and_composition.task5;
 
+import java.util.Scanner;
+
 public class Main {
-    
-    public static void main(String[] args) {
-              
-        TourAgency tourAgency = new TourAgency();
-        
-        int choice, from, to;
-        
-        while (true) {
-            menuText();
-            choice = TourAgency.numInput();
-            if (choice == 0)
-                break;
-            if (choice < 0 || choice > 4) {
-                System.out.println("Incorrect choose!");
-                continue;
-            }
-                      
-            switch (choice) {
-                case 1:
-                    System.out.println("List of clients:");
-                    tourAgency.showClients();
-                    break; 
-                case 2:
-                    System.out.println("List of tours:");
-                    tourAgency.showTours();
-                    break;
-                case 3:
-                    System.out.println("List of tours by cost: ");
-                    System.out.print("From: ");
-                    from = TourAgency.numInput();
-                    System.out.print("To: ");
-                    to = TourAgency.numInput();
-                    tourAgency.toursByCost(from, to);
-                    break; 
-                case 4:
-                    System.out.println("List of tours by transport, food, days: ");
-                    System.out.print("Transport: ");
-                    String transport = TourAgency.strInput();
-                    System.out.print("Food (1 - true, 0 - false): ");
-                    int food = TourAgency.numInput();
-                    System.out.println("Number of days: ");
-                    System.out.print("From: ");
-                    from = TourAgency.numInput();
-                    System.out.print("To: ");
-                    to = TourAgency.numInput();
-                    tourAgency.toursByTransportFoodDays(transport, food, from, to);
-                    break;
-            }     
-            
-        }
-        
+
+	public static void main(String[] args) {
+
+		TourAgency tourAgency = new TourAgency();
+		TourAgencyLogic tourAgencyLogic = new TourAgencyLogic();
+		TourAgencyView tourAgencyView = new TourAgencyView();
+
+		Client petya = new Client("Petya", 59, "+37529 357-32-59");
+		Client ivan = new Client("Ivan", 18, "+37529 218-45-14");
+
+		tourAgency.addTour(new Tour(TourType.CRUISE, TourTransport.SHIP, true, 14, 1000));
+		tourAgency.addTour(new Tour(TourType.TREATMENT, TourTransport.TRAIN, true, 10, 500));
+		tourAgency.addTour(new Tour(TourType.EXCURSION, TourTransport.BUS, false, 5, 350));
+		tourAgency.addTour(new Tour(TourType.SHOPPING, TourTransport.BUS, false, 3, 200));
+		tourAgency.addTour(new Tour(TourType.RECREATION, TourTransport.AIRPLANE, true, 10, 1200));
+
+		int choice;
+
+		while (true) {
+
+			menuText();
+			choice = getIntFromConsol("");
+
+			if (choice < 0 || choice > 3) {
+				System.out.println("Incorrect choose!");
+				continue;
+			}
+
+			if (choice == 0)
+				break;
+			switch (choice) {
+			case 1:
+				tourAgencyLogic.sortToursByCost(tourAgency);
+				tourAgencyView.printTours(tourAgency.getTours());
+				break;
+			case 2:
+				tourAgencyLogic.sortToursByDays(tourAgency);
+				tourAgencyView.printTours(tourAgency.getTours());
+				break;
+			case 3:
+				System.out.println("Tours for " + petya + ":");
+				tourAgencyView.printTours(tourAgencyLogic.pickUpTours(TourTransport.TRAIN, true, 7, 14, 300, 600, tourAgency));
+				System.out.println();
+				System.out.println("Tours for " + ivan + ":");
+				tourAgencyView.printTours(tourAgencyLogic.pickUpTours(TourTransport.SHIP, true, 7, 14, 800, 1500, tourAgency));
+				break;
+			}
+
+		}
+
+	}
+
+	public static void menuText() {
+          System.out.print(
+	            "\n" + 
+	            "0 - Exit\n" + 
+	            "1 - View all tours by cost\n" +   
+	            "2 - View all tours by days\n" + 
+	            "3 - View all tours for client\n" + 
+	            "\n"
+	      ); 
     }
-    
-    private static void menuText() { 
-        System.out.print(
-            "\n" + 
-            "0 - Exit\n" +
-            "1 - List of clients\n" +   
-            "2 - List of tours\n" +   
-            "3 - List of tours by value\n" + 
-            "4 - List of tours by transport, food, number of days\n" + 
-            "\n"
-        );   
-    } 
-    
+
+	public static int getIntFromConsol(String str) {
+
+		int a;
+
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print(str);
+		while (!scanner.hasNextInt()) {
+			scanner.nextLine();
+			System.out.print(str);
+		}
+		a = scanner.nextInt();
+
+		return a;
+
+	}
+
 }
